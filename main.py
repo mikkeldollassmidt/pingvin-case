9f51b92bb47b588e4778bc2d26ce68c8c74d34ab
Mikkel Dollas <mikkeldollaskontakt@gmail.com>
2025-11-10 13:45:49 +0100

    Changed dataset retrieving, specifying outcome

--- main.py
+++ main.py
@@ -3,14 +3,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 import joblib
-
-# Importing and loading dataset into data variable.
 import seaborn as sns
-data = sns.load_dataset("penguins")
 
 def main():
-    # Indlæs og kig på data
-    print(data.isna().sum())
+    
+    # Loading dataset.
+    data = sns.load_dataset("penguins")
+
+    #####################################
+    # Initialize and look at the data   #
+    #####################################
+    
+    # print(data.head())
+    # print(data.info())
+    # print(data.describe())
+    # print(data.isna().sum())
+    
+    
+    # Remove rows with missing values
+    # print(data.dropna())
+    
+    # Specify the outcome
+    features = ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g", "sex"]
+    target = "species"
+    data = data[features + [target]]
+    print(data.head())
 
 
 if __name__ == "__main__":
