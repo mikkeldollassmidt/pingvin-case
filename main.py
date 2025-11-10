14074a8e1a2321f28b1ebdc3af206ede7f036151
Mikkel Dollas <mikkeldollaskontakt@gmail.com>
2025-11-10 15:52:11 +0100

    New tasks

--- main.py
+++ main.py
@@ -4,35 +4,53 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 import joblib
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
 
 def main():
-    
+
     # Loading dataset.
     data = sns.load_dataset("penguins")
 
     #####################################
     # Initialize and look at the data   #
     #####################################
-    
+
     # print(data.head())
     # print(data.info())
     # print(data.describe())
     # print(data.isna().sum())
-    
-    
+
     # Remove rows with missing values
     # print(data.dropna())
-    
+
     # Specify the outcome
     # features = ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g", "sex"]
     # target = "species"
     # data = data[features + [target]]
     # print(data.head())
-    
-    # Hot Encode
-    data = pd.get_dummies(data, drop_first=True)
-    print(data.head())
 
+    # Hot Encode
+    # data = pd.get_dummies(data, drop_first=True)
+    # print(data.head())
+
+    # Split Data
+    features = ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g", "sex"]
+    target = "species"
+
+    X = data[features]
+    y = data[target]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=69
+    )
+
+    print("Træningsdata:", X_train.shape)
+    print("Testdata:", X_test.shape)
 
 if __name__ == "__main__":
     main()
