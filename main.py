a4f167d19a47dbbec5789c154bfa5773313fe1ad
Mikkel Dollas <mikkeldollaskontakt@gmail.com>
2025-11-11 08:58:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     
-    model_tree = DecisionTreeClassifier(max_depth=3, random_state=42)
+    model_tree = DecisionTreeClassifier(max_depth=1, random_state=42)
     model_tree.fit(X_train, y_train)
 
 
