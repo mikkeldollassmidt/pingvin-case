c1d2dd32e9b9db4606fda2c350d809f4862ac204
Mikkel Dollas <mikkeldollaskontakt@gmail.com>
2025-11-11 08:43:02 +0100

    Added new tasks

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 
 def main():
@@ -39,18 +39,55 @@ def main():
     # print(data.head())
 
     # Split Data
+    # features = ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g", "sex"]
+    # target = "species"
+
+    # X = data[features]
+    # y = data[target]
+
+    # X_train, X_test, y_train, y_test = train_test_split(
+    #     X, y, test_size=0.2, random_state=69
+    # )
+
+    # print("Træningsdata:", X_train.shape)
+    # print("Testdata:", X_test.shape)
+
+    # Train the machine
+    data = data.dropna()
+    
+    le_sex = LabelEncoder()
+    data["sex"] = le_sex.fit_transform(data["sex"])
+
+    le_species = LabelEncoder()
+    data["species"] = le_species.fit_transform(data["species"])
+    
     features = ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g", "sex"]
     target = "species"
+
 
     X = data[features]
     y = data[target]
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=69
-    )
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    print("Træningsdata:", X_train.shape)
-    print("Testdata:", X_test.shape)
+    
+    model_tree = DecisionTreeClassifier(max_depth=3, random_state=42)
+    model_tree.fit(X_train, y_train)
+
+
+    # Evaluating data
+    y_pred_tree = model_tree.predict(X_test)
+    print("1. DT Accuracy:", round(accuracy_score(y_test, y_pred_tree), 2))
+    print("2. DT Confusion Matrix:\n", confusion_matrix(y_test, y_pred_tree))
+    print("3. DT Classification Report:\n", classification_report(y_test, y_pred_tree)) 
+       
+    model_knn = KNeighborsClassifier(n_neighbors=5)
+    model_knn.fit(X_train, y_train)
+    y_pred_knn = model_knn.predict(X_test)
+    print("1. KNN Accuracy:", round(accuracy_score(y_test, y_pred_knn), 2))
+    print("2. KNN Confusion Matrix:\n", confusion_matrix(y_test, y_pred_knn))
+    print("3. KNN Classification Report:\n", classification_report(y_test, y_pred_knn))
+
 
 if __name__ == "__main__":
     main()
